c773106df3f287a927a9c15b31b4f604da86226e
HaroldNdougou <106807099+HaroldNdougou@users.noreply.github.com>
2024-06-25 09:01:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const playerDisplay = document.getElementById("player-choice");
+const computerDisplay = document.getElementById("computer-choice");
+const resultDisplay = document.getElementById("resultDisplay");
+
+const choices = ["Rock","Paper","Scissors"];
+
+function Game(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    const getResult = (playerChoice, computerChoice) =>
+        playerChoice === computerChoice ? "IT'S A TIE! 🤔" :
+        (playerChoice === "Rock" && computerChoice === "Scissors") ||
+        (playerChoice === "Paper" && computerChoice === "Rock") ||
+        (playerChoice === "Scissors" && computerChoice === "Paper") ?
+        "YOU WIN 😊"
+        : "YOU LOSE 😌";
+
+        const result = getResult(playerChoice,computerChoice);
+        resultDisplay.textContent = result;
+        playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+        computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+
+        resultDisplay.style.backgroundColor =
+        result === "IT'S A TIE! 🤔" ? "blue"
+        : result === "YOU WIN 😊" ? "green"
+        :"red";
+        resultDisplay.style.border = "2px sild transparent"
+        resultDisplay.style.color = "white";
+        
+}
